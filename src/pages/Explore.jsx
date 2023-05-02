672d2657f47e510d2c12636e96ee1e868e4be6e1
Charles Ukadike <charlesukadike44@gmail.com>
2023-05-02 20:52:57 +0100

    optimization test

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -9,7 +9,7 @@ const catData = [
         23 Gele Style <br /> Ideas
       </>
     ),
-    src: "../assets/images/gele.jpg" /*geleData[14]*/
+    src: "./images/gele.jpg" /*geleData[14]*/
   },
   {
     id: 2,
@@ -18,7 +18,7 @@ const catData = [
         Hair Style <br /> Ideas
       </>
     ),
-    src: "../assets/images/hair.jpg" /*"https://images2.imgbox.com/35/71/5fsl1tFm_o.jpg"*/
+    src: "./images/hair.jpg" /*"https://images2.imgbox.com/35/71/5fsl1tFm_o.jpg"*/
   }
 ];
 
